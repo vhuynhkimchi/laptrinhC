989d2c3e40611fcd8d887878e96c2735fcb4a91a
vhuynhkimchi <162584036+vhuynhkimchi@users.noreply.github.com>
2024-04-22 11:01:12 +0700

    Add files via upload

---
+++ Cau1De4.cpp
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#define maxd 10
+#include<limits.h>
+
+int n;
+int mt[maxd][maxd];
+
+void NhapHangCot()
+{
+	printf("Nhap n=hang=cot: ");
+	scanf("%d",&n);
+	
+	while (n < 2 || n > 10)
+	{
+		printf("Nhap lai n: ");
+		scanf("%d", &n);
+	}
+}
+
+void NhapMang()
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			scanf("%d", &mt[i][j]);
+		}
+	}
+}
+
+void XuatMang()
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			printf("%d\t", mt[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int Min(int m[][maxd], int n)
+{
+	int min = INT_MAX;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{	
+			if(i < j && min > mt[i][j]){
+				min = mt[i][j];
+			}
+		}
+	} 
+	return min;
+}
+
+void cau3(){
+	int min = Min(mt,n);
+	int dem=0;
+	
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{	
+			if(i < j && min == mt[i][j]){
+				dem++;
+			}
+		}
+	} 
+	printf("Co %d phan tu co gia tri cung nho nhat la %d", dem, min);
+}
+
+int kt_giam(int m[][maxd], int n, int d)
+{
+	for(int i=0; i<n-1; i++){//cac phan tu cua cot
+		if(mt[d][i] < mt[d][i+1]){
+			return 0;//vi pham quy tac khong giam -> return 0
+		}	
+	}
+	return 1;//het for ma ko vi pham quy tac -> return 1
+}
+
+int main()
+{
+	NhapHangCot();
+	NhapMang();
+	printf("\nMang vua nhap:\n");
+	XuatMang();
+	Min(mt, n);
+	cau3();
+	int d=2;
+	if(kt_giam(mt,n,d)==1){
+		printf("\nDong thu %d, cac phan tu da sap xep theo thu tu giam dan",d);
+	}
+	else{
+		printf("\nDong thu %d, cac phan tu khong sap xep theo thu tu giam dan",d);
+	}
+}
---
+++ cau 1 de 1.cpp
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#define maxd 5
+#include<limits.h>
+int n;
+int c;
+int a[maxd][maxd];
+
+
+void nhapsohangcot(){
+	printf("Nhap hang=cot=n:");
+	scanf("%d",&n);
+	while(n<2||n>15){
+		printf("Nhap lai n:");
+		scanf("%d",&n);
+	}
+}
+void nhapphantu(){
+	for(int i=0; i<n; i++){
+		for(int j=0;j<n;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+}
+void xuatmang(){
+	for(int i=0; i<n; i++){
+		for(int j=0;j<n;j++){
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+}
+int ktra_chan(int x){
+	if(x%2==0){
+		return 1;
+	}
+	return 0;
+}
+float inchan(int m[][maxd], int n){
+	float tbc=0;
+	int tong=0;
+	float cnt=0;
+	for(int i=0; i<n; i++){
+		for(int j=0;j<n;j++){
+			if(i+j<n-1 && ktra_chan(a[i][j])==1){	
+				tong+=a[i][j];
+				cnt++;
+			}
+		}
+	}
+	tbc=tong/cnt;
+	return tbc;
+}
+int min(int m[][maxd], int n){
+	int min=INT_MAX;
+		for(int i=0; i<n; i++){
+			for(int j=0;j<n;j++){
+				if(i<j && min>a[i][j]){
+					min=a[i][j];
+				}
+			}
+		}
+		return min;
+}
+
+int caud()
+{
+	if (n == 15)
+	{
+		printf("\nNhap c:");
+		scanf("%d",&c);
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==j)
+				{
+					printf("%d ",c);
+				}
+				else
+				{
+					printf("0 ");
+				}
+			}
+			printf("\n");
+		}
+	}
+}
+
+int main(){
+	nhapsohangcot();
+	nhapphantu();
+	xuatmang();
+	printf("TBC=%.2f",inchan(a,n));
+	printf("\nGTNN tren duong cheo chinh: %d",min(a,n));
+	caud();
+}
+
