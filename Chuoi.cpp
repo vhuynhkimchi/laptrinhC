fc2c76a60edbaa728d98f431678b9bcb594667bf
vhuynhkimchi <162584036+vhuynhkimchi@users.noreply.github.com>
2024-05-21 19:38:07 +0700

    Add files via upload

--- Chuoi.cpp
+++ Chuoi.cpp
@@ -1,56 +1,107 @@
 #include<stdio.h>
 #include<string.h>
 #define maxd 100
-char s[maxd] = "thang gieng la thang an choi, thang hai co bac, thang ba ruoi che";
 
-int DemNA(char m[maxd])
+char s[maxd];
+int len=0;
+
+void Nhap()
+{
+	printf("Nhap chuoi: ");
+	gets(s);
+}
+
+int dodai(char m[maxd])
 {
 	int dem = 0;
-	char a[maxd] = "ueoaiUEOAI";
-	for(int i = 0; i < strlen(m); i++)
+	int i = 0;
+	while(m[i] != '\0')
 	{
-		for(int j = 0; j < strlen(a); j++)
+		dem++;
+		i++;
+	}
+	
+	return dem;
+}
+
+void In(char m[maxd])
+{
+	printf("Chuoi da nhap la:");
+	printf("%s",s);
+	len=dodai(m);
+	printf("\nChuoi co do dai = %d",len);
+}
+
+
+void DemNA()
+{
+	int dem = 0;
+	char b[maxd] = "UEOAIueoai";
+	for(int i = 0; i < len; i++)
+	{
+		for(int j = 0; j < strlen(b); j++)
 		{
-			if(m[i] == a[j])
+			if(s[i] == b[j])
 			dem++;
 		}
 	}
-	return dem;
-}
-void tudainhat()
-{
-	int i;
-	int a[maxd];
-	int dem = 0;
-	int len = 0;
-	while(s[i] != '\0' )
-	{
-		if(s[i] != ' ')
-		{
-			len++;
-		}
-		else
-		{
-			a[dem++] = len;
-			len = 0;
-		}
-		i++;
-	}
-	int max = 0;
-	for(int i = 0; i < dem; i++)
-	{
-		if(a[i] > max)
-		{
-			max = a[i];
-		}
-	}
-	printf("\nTu dai nhat trong chuoi co %d ky tu",max);
-}
-int main()
-{
-	printf("Chuoi da nhap la :");
-	printf("%s",s);
-	printf("\nCo %d nguyen am trong chuoi.", DemNA(s));
-	tudainhat();
+	printf("\nSo luong nguyen am trong chuoi: %d",dem);
 }
 
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+void my_strrev()
+{
+	char des[maxd];
+	int dem = 0;
+
+	for(int i = len-1; i >= 0; i--)
+	{
+		if(s[i] != '\0'){
+			des[dem++] = s[i];
+		}
+	}
+	des[dem]='\0';
+	printf("\nMy_strrev: %s",des);
+}
+
+void Daonguoc()
+{
+	char des[maxd]="";
+	int dem = 0;
+	printf("\nIn moi tu mot dong: \n");
+	for(int i = len-1; i >= 0; i--)
+	{
+		if(s[i]!=' ' && s[i]!='\0'){
+			des[dem++] = s[i];
+		}
+		else{//s[i]=' '
+			//printf("%s",des);
+			for(int i=dem-1; i>=0; i--){
+				printf("%c",des[i]);
+			}
+			printf("\n");
+			dem=0;
+		}
+	}
+	//in tu dau tien
+	int i=0;
+	while(s[i]!=' '){
+		printf("%c",s[i]);
+		i++;
+	}
+}
+
+int main()
+{
+	Nhap();
+	In(s);
+	DemNA();
+	my_strrev();
+	Daonguoc();
+}
